a906ce368a46fc7a36a884b0a4e5b75329c28802
onuralpArlsn <onuralparslan97@gmail.com>
2025-04-29 23:27:22 +0300

    fixed connection

--- Hw_Week2/connecttoneo4j.py
+++ Hw_Week2/connecttoneo4j.py
@@ -1,10 +1,14 @@
 from neo4j import GraphDatabase
 from dotenv import load_dotenv
 load_dotenv()  # take environment variables
+import os
+
+USERNAME_NEO= os.getenv('USERNAME_NEO')
+PASSWORD = os.getenv('PASSWORD')
 
 # URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
-URI = "neo4j+s://f750ff4b.databases.neo4j.io"
-AUTH = ("<Username>", "<Password>")
+URI = "neo4j+s://a5d568d8.databases.neo4j.io"
+AUTH = (USERNAME_NEO, PASSWORD)
 
 with GraphDatabase.driver(URI, auth=AUTH) as driver:
     driver.verify_connectivity()
